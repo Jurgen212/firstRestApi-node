cb9f30ed21393da87f277daafd9ec96a0c8e03f6
Jurgen212 <jurgensanclemente@gmail.com>
2022-07-05 12:10:09 -0500

    Primer commit restApi

---
+++ controllers/usuarios.js
@@ -0,0 +1,70 @@
+
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response ) => {
+
+
+
+    const { q, nombre = 'No name', apiKey } = req.query;
+
+    res.json({
+        msg : "Get API",
+        q              ,
+        nombre         ,
+        apiKey
+    });s
+};
+
+
+
+const usuariosPut = (req, res = response ) => {
+
+    const { id }  = req.params;
+     
+    res.status( 500 ).json({
+        msg : "put API",
+        id             
+    });
+
+};
+
+
+
+
+const usuariosPost = (req, res = response ) => {
+
+
+    const { nombre, edad } = req.body;
+
+    res.status(201).json({
+        msg : "post API",
+        nombre          ,
+        edad
+    });
+};
+
+
+
+const usuariosDelete =  (req, res = response ) => {
+
+    res.json({
+        msg : "delete API"
+    });
+};
+
+
+const usuariosPatch = (req, res = response ) => {
+
+    res.json({
+        msg : "Patch API"
+    });
+}
+
+
+module.exports = {
+    usuariosGet     ,
+    usuariosPut     ,
+    usuariosPost    ,
+    usuariosDelete  ,
+    usuariosPatch
+};
